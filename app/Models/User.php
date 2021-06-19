b0d5bd5fd93c077a4048e3f14144728525988763
nguyen <dangnguyen2739@gmail.com>
2021-06-19 10:50:37 +0700

    week13

---
+++ app/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+    protected $primaryKey = 'user_id';
+    protected $with = ['groups','permissions'];
+    public function getAuthPassword()
+    {
+        return $this->user_password;
+    }
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_name',
+        'user_email',
+        'user_password',
+        'user_phone',
+        'user_avatar',
+        'status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    public function groups()
+    {
+        return $this->hasMany(Group::class,'group_id');
+    }
+
+    public function permissions()
+    {
+        return $this->hasMany(Permission::class,'permission_id');
+    }
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
